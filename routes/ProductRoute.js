344771a5c0d4965c4bd31a41b189cf11a61b9741
Marcellinus <itsmarsel@gmail.com>
2024-06-10 16:09:22 +0700

    delete route in product

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -3,16 +3,6 @@ import { createProduct, deleteProduct, getCategoriesByProductId, getProductById,
 
 const router = express.Router();
 
-// router.get("/products/search", getProducts); // Updated route for search by name
-// router.get("/products/search/:name", searchProductsByName);
-// router.get('/products', getProducts);
-// router.post('/products', createProduct);
-// router.put('/products/:id', updateProduct);
-// router.delete('/products/:id', deleteProduct);
-// router.get('/products/:id', getProductById);
-router.get('/categories/product/:product_id', getCategoriesByProductId);
-router.get('/products/category/:category_id', getProductsByCategoryId);
-
 // Bikin Best Practice
 router.get('/products', getProducts);
 router.post('/products', createProduct);
